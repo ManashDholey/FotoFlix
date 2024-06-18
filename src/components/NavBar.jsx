f74521b2bfcf2a1c7ca46488016650b0b22ce085
Manash <Manash@DESKTOP-H7BTIBJ>
2024-06-18 21:00:30 +0530

    commit for css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+
+
 const NavBar = ({query,handleSubmit,setQuery}) => {
+    const [active, setActive] = useState(null);
+    const [dataTabs, setDataTabs] = useState([
+        {
+          id: 1,
+          tabTitle: "Home",
+          tabClass: "myCustomClass",
+          tabClicked: false,
+          navigateTo:"/"
+        },
+        {
+          id: 2,
+          tabTitle: "Favorites",
+          tabClass: "myCustomClass",
+          tabClicked: false,
+          navigateTo:"/favorites"
+        }
+      ]);
+      const navigate = (id) => {
+        
+        setActive(id);
+      };
+  
+  const NavLink = ({ item, isActive, onClick }) => {
+    return (
+        <Link to={item.navigateTo} className={isActive ? 'active': ''}  onClick={()=>{onClick(item.id)}}> {item.tabTitle } </Link>
+    );
+  };
   return (
-    <div>
+    <div className='topnav'>
           <nav className="navbar">
             <div className="nabbar_logo">
               Fotoflix
@@ -17,8 +46,13 @@ const NavBar = ({query,handleSubmit,setQuery}) => {
            </form>
             </div>
             <div className="nabbar_links">
-              <Link to="/"  > Home </Link>
-              <Link to="/favorites">Favorites</Link>
+            {dataTabs.map((item) => (
+        <div key={item.id} >
+          <NavLink item={item} isActive={active === item.id} onClick={navigate} />
+        </div>
+      ))}
+              {/* <Link to="/" className={isActive ? 'active': ''}  onClick={toggleClass}> Home </Link>
+              <Link to="/favorites" className={isActive ? 'active': ''}  onClick={toggleClass}>Favorites</Link> */}
             </div>
           </nav>
         </div>
